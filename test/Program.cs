cbf87efdd32452b758c33de110154524a83a3fe8
FIC <bruno_afonso95@hotmail.com>
2017-10-05 21:05:05 -0300

    test

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleCadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FazerCafe(" caramelo");
+            FazerCafe(" chocolate");
+        }
+        static void FazerCafe(string capsula)
+        {
+            Console.WriteLine("Esquentar agua");
+            Console.WriteLine("Colocar po de magia (Cafe)");
+            Console.WriteLine("Colocar" + capsula);
+        }
+    }
+}
